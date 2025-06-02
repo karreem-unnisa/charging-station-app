c5dec26644937b3c6bcd18d1490254a757b3a403
Kareemunnisa <mailkareemunnisa@gmail.com>
2025-06-02 22:19:40 +0530

    server update

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,14 @@
     connectDB();
 
     const app = express();
-    app.use(cors());
+    const corsOptions = {
+  origin: ['https://charging-station-app-zn3m.onrender.com'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+};
+
+app.use(cors(corsOptions));
+
     app.use(express.json());
 
     app.use('/api/auth', authRoutes);
